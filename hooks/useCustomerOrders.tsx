b8a996c488c6722cb8a3ad07c4acbaf8ecbeee77
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-11-11 13:20:20 +0530

    chnages in hooks file changes in hooks file

--- hooks/useCustomerOrders.tsx
+++ hooks/useCustomerOrders.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { GET_ORDERS } from "../graphql/queries";
 
 const useCustomerOrders = () => {
-  const { loading, error, data } = useQuery(GET_ORDERS);
+  const { loading, error, data } = useQuery( GET_ORDERS );
 
   return <div>useCustomerOrders</div>;
 };
